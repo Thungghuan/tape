50b85890127704cd7395d6fe0d120f60c71e0fb2
Thungghuan <1092887426@qq.com>
2020-12-25 02:01:28 +0800

    feat:add some route files

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const http = require("http");
+// const path = require("path");
+// const bodyParser = require("body-parser");
+const config = require("../config/index");
+
+const app = express();
+const server = http.createServer(app);
+
+const port = config.server_port;
+
+// const questionsRouter = require('./routes/questions');
+// const loginRouter = require('./routes/login');
+// const allQuestionsRouter = require('./routes/allQuestions');
+// const answerRouter = require('./routes/answer');
+
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//   res.send("Test successfully")
+// })
+
+// app.use('/questions', questionsRouter);
+// app.use('/login', loginRouter);
+// app.use('/allQuestions', allQuestionsRouter);
+// app.use('/answer', answerRouter);
+
+app.set("port", port);
+
+server.listen(port);
