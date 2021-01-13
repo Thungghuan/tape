760ebd16f9c0ae1ff874aac95009835d98d24e50
Thungghuan <1092887426@qq.com>
2021-01-13 21:23:37 +0800

    feat: initialize home page DOM

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,47 @@
 <template>
-  <div id="nav">
+  <div id="background">
+    <img
+      src="https://pic1.zhimg.com/80/v2-6c5e3fcffd92c1149c28e1d075881043_720w.jpg?source=1940ef5c"
+    />
+  </div>
+  <div id="wrapper">
+    <div class="ask-wrapper">
+      11
+    </div>
+    <div class="question-wrapper">
+      <div class="title"></div>
+      <Question />
+      <Question />
+      <Question />
+      <Question />
+      <Question />
+      <Question />
+    </div>
+  </div>
+  <!-- <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view />
+  <router-view /> -->
 </template>
 
+<script>
+// @ is an alias to /src
+import Question from "@/components/Question.vue";
+
+export default {
+  components: {
+    Question
+  }
+};
+</script>
+
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -15,7 +50,42 @@
   color: #2c3e50;
 }
 
-#nav {
+#background {
+  position: fixed;
+  z-index: -1;
+}
+
+#background img {
+  width: 100vw;
+  height: auto;
+}
+
+#wrapper {
+  width: 90vw;
+  margin: 0 auto;
+  padding-top: 15vh;
+}
+
+.ask-wrapper {
+  width: 70vw;
+  height: 25vh;
+  margin: 0 auto;
+  background-color: red;
+}
+
+.question-wrapper {
+  width: 80vw;
+  margin: 0 auto;
+  background-color: black;
+  margin-top: 20vh;
+}
+
+.title {
+  height: 200px;
+  background-color: #ff0;
+}
+
+/* #nav {
   padding: 30px;
 }
 
@@ -26,5 +96,5 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
---
+++ src/components/Question.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="question"></div>
+</template>
+
+<script>
+export default {
+  name: "Question"
+};
+</script>
+
+<style scoped>
+.question {
+  width: 80vw;
+  height: 10vh;
+  background-color: #00f;
+  margin-top: 10px;
+}
+</style>
