3fd186d6a2164bac5a5f5caa8a1a9fc066cee40d
Thungghuan <1092887426@qq.com>
2021-01-16 14:27:25 +0800

    feat: add home page route

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,15 @@
 <template>
-  <div id="background">
-    <img
-      src="https://pic1.zhimg.com/80/v2-6c5e3fcffd92c1149c28e1d075881043_720w.jpg?source=1940ef5c"
-    />
-  </div>
-  <div id="wrapper">
-    <ask-wrapper />
-    <question-wrapper />
-  </div>
-  <!-- <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view /> -->
+  <!-- <div id="nav"> -->
+  <!-- <router-link to="/">Home</router-link> | -->
+  <!-- <router-link to="/about">About</router-link> -->
+  <!-- </div> -->
+  <router-view />
 </template>
 
 <script>
 // @ is an alias to /src
-import AskWrapper from "@/components/AskWrapper.vue";
-import QuestionWrapper from "@/components/QuestionWrapper.vue";
 
-export default {
-  components: {
-    AskWrapper,
-    QuestionWrapper
-  }
-};
+export default {};
 </script>
 
 <style>
@@ -40,22 +24,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#background {
-  position: fixed;
-  z-index: -1;
-}
-
-#background img {
-  width: 100vw;
-  height: auto;
-}
-
-#wrapper {
-  width: 90vw;
-  margin: 0 auto;
-  padding-top: 25vh;
 }
 
 /* #nav {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="background">
+    <img
+      src="https://pic1.zhimg.com/80/v2-6c5e3fcffd92c1149c28e1d075881043_720w.jpg?source=1940ef5c"
+    />
+  </div>
+  <div id="wrapper">
+    <ask-wrapper />
+    <question-wrapper />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import AskWrapper from "@/components/AskWrapper.vue";
+import QuestionWrapper from "@/components/QuestionWrapper.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    AskWrapper,
+    QuestionWrapper
   }
 };
 </script>
+
+<style>
+#background {
+  position: fixed;
+  z-index: -1;
+}
+
+#background img {
+  width: 100vw;
+  height: auto;
+}
+
+#wrapper {
+  width: 90vw;
+  margin: 0 auto;
+  padding-top: 25vh;
+}
+</style>
